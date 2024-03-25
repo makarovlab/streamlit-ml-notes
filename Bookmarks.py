5ce404f88509664076aacf2c45fd78016a7f5864
makarovlab <mmakarovlab@gmail.com>
2024-03-25 22:11:34 +0100

    add bookmarks

---
+++ Bookmarks.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+   page_title="Bookmarks",
+   page_icon="🧊",
+   layout="wide",
+   initial_sidebar_state="expanded",
+)
+
+st.header("Bookmarks")
+
+st.subheader("English")
+st.markdown("[Orange Visual programming](https://orange3.readthedocs.io/projects/orange-visual-programming/en/latest/index.html#)")
+st.markdown("[Clustering Text in Natural Language Processing: Unveiling Patterns and Insights](https://medium.com/the-modern-scientist/clustering-text-in-natural-language-processing-unveiling-patterns-and-insights-8c3cd137b135#:~:text=Text%20Clustering%20Methodologies&text=K%2DMeans%20Clustering%3A%20K%2D,a%20group%20of%20similar%20documents.)")
+st.subheader("Russian")
